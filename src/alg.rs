48d5842f6c787b07210038807d9ad2e9bcb957b3
dvshapkin <dvshapkin@mail.ru>
2019-06-05 12:54:07 +0700

    изменена структура модулей

--- src/alg.rs
+++ src/alg.rs
@@ -1,14 +1,2 @@
-pub fn quick_sort<T>(seq: &mut [T])
-    where T: PartialOrd
-{
-    let middle = &seq[seq.len() / 2];
-    let mut i = 0_usize;
-    let mut j = seq.len() - 1;
-    while i <= j {
-        while seq[i] < *middle { i += 1 }
-        while seq[j] > *middle { j -= 1 }
-        if i <= j {
-            std::mem::swap(&mut seq[i], &mut seq[j]);
-        }
-    }
-}
+pub mod search;
+pub mod sort;
---
+++ src/alg/search.rs
@@ -0,0 +1,3 @@
+pub fn binary<T>(seq: &[T]) -> Option<usize> {
+    None
+}
