8817459e7cf19c70ce4f736b90dd496789d4a1da
dvshapkin <dvshapkin@mail.ru>
2019-06-06 19:20:18 +0700

    binary search

--- src/alg/search.rs
+++ src/alg/search.rs
@@ -1,21 +1,28 @@
 /// Binary search in slice.
 ///
-pub fn binary<T>(list: &[T], item: T) -> Option<usize>
+pub fn binary<T>(list: &[T], item: &T) -> Option<usize>
     where
-        T: PartialOrd,
+        T: PartialOrd
 {
-    if list.len() == 0 {
-        return None;
-    }
+    return partial(&list, 0, list.len(), item);
 
-    let mid = list.len() / 2;
+    fn partial<T>(list: &[T], lhs: usize, rhs: usize, item: &T) -> Option<usize>
+        where
+            T: PartialOrd
+    {
+        if list[lhs..rhs].len() == 0 {
+            return None;
+        }
 
-    if list[mid] > item {
-        return binary(&list[..mid], item);
-    } else if list[mid] < item {
-        return binary(&list[mid..], item);
-    } else {
-        return Some(mid);
+        let mid = list[lhs..rhs].len() / 2;
+
+        if list[mid] > *item {
+            return partial(&list, lhs, mid, item);
+        } else if list[mid] < *item {
+            return partial(&list, mid, rhs, item); //binary(&list[mid..], item);
+        } else {
+            return Some(mid);
+        }
     }
 }
 
@@ -23,7 +30,14 @@ pub fn binary<T>(list: &[T], item: T) -> Option<usize>
 mod tests {
     #[test]
     fn binary_integer() {
-        let list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-        assert_eq!(super::binary(&list, 7).unwrap(), 6);
+        //let list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        let list = [7];
+        assert_eq!(super::binary(&list, &7).unwrap(), 0);
+        assert_eq!(super::binary(&list, &1), None);
+
+        let list = [7,8,9];
+        assert_eq!(super::binary(&list, &7).unwrap(), 0);
+        assert_eq!(super::binary(&list, &8).unwrap(), 1);
+        assert_eq!(super::binary(&list, &9).unwrap(), 2);
     }
 }
