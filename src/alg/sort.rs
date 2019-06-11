d2288ef3ec91ad454c6ec479c0626ab6a30f037e
dvshapkin <dvshapkin@mail.ru>
2019-06-11 15:47:36 +0700

    selection() - bidirectional sort ok

--- src/alg/sort.rs
+++ src/alg/sort.rs
@@ -2,17 +2,27 @@ use super::search;
 
 /// Selection sort.
 ///
-pub fn selection<T>(list: &mut [T])
-    where T: PartialOrd
+pub fn selection<T>(list: &mut [T], ascending: bool)
+    where
+        T: PartialOrd
 {
     if list.len() < 2 {
         return;
     }
+
+    let extrem = if ascending { search::min } else { search::max };
+    let compare: fn(&T, &T) -> bool = if ascending {
+        |a,b| a < b
+    } else {
+        |a,b| a > b
+    };
+
     let mut cur_pos = 0;
     let max_idx = list.len() - 1;
+
     while cur_pos < max_idx {
-        let idx = search::min(&list[cur_pos..]).unwrap() + cur_pos;
-        if idx > cur_pos {
+        let idx = extrem(&list[cur_pos..]).unwrap() + cur_pos;
+        if compare(&list[idx], &list[cur_pos]) {
             list.swap(cur_pos, idx);
         }
         cur_pos += 1;
@@ -36,10 +46,16 @@ mod tests {
 
     #[test]
     fn selection_ok() {
-        let mut list = [1, 2, 12, 5, 43, 21, 0, 34, 100, 3];
-        selection(&mut list);
+        let mut list = [1, 2, 12, 5, 43, 21, 0, 2, 34, 100, 3];
+
+        selection(&mut list, true);
         for i in 0..list.len() - 1 {
             assert!(list[i] <= list[i + 1]);
+        }
+
+        selection(&mut list, false);
+        for i in 0..list.len() - 1 {
+            assert!(list[i] >= list[i + 1]);
         }
     }
 }
